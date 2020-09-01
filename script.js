0aef524efa2984e77d67d7243cb0437c77685cf5
clairetay96 <clairetay96@gmail.com>
2020-09-01 20:43:16 +0800

    All done, bootstrap and flex used

---
+++ script.js
@@ -0,0 +1,31 @@
+function alertFunc(){
+    alert("This feature has not been added yet.")
+}
+
+document.querySelector("#guidebooks").addEventListener("click", alertFunc)
+
+document.querySelector("#destinations").addEventListener("click", alertFunc)
+
+document.querySelector("#box1").addEventListener("click", function(){
+    window.open("https://www.google.com/maps/place/San+Francisco,+CA,+USA/data=!4m2!3m1!1s0x80859a6d00690021:0x4a501367f076adff?sa=X&ved=2ahUKEwjEiLP3-8frAhVTfX0KHZPxC9sQ8gEwK3oECB0QBg")
+})
+
+document.querySelector("#box2").addEventListener("click", function(){
+    window.open("https://www.google.com/maps/place/New+York,+NY,+USA/@40.6976684,-74.2605611,10z/data=!3m1!4b1!4m5!3m4!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728")
+})
+
+document.querySelector("#box3").addEventListener("click", function(){
+    window.open("https://www.google.com/maps/place/London,+UK/@51.5287336,-0.3824714,10z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5073509!4d-0.1277583")
+})
+
+document.querySelector("#box4").addEventListener("click", function(){
+    window.open("https://www.google.com/maps/place/Napa,+CA,+USA/@38.2860764,-122.3601707,12z/data=!3m1!4b1!4m8!1m2!2m1!1snapa!3m4!1s0x8084ffe7f8f2deef:0xd6629f3a3384c725!8m2!3d38.2975381!4d-122.286865")
+})
+
+document.querySelector("#box5").addEventListener("click", function(){
+    window.open("https://www.google.com/maps/place/Sonoma,+CA+95476,+USA/@38.2911555,-122.4842049,13z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356")
+})
+
+document.querySelector("#box6").addEventListener("click", function(){
+    window.open("https://www.google.com/maps/place/San+Francisco,+CA,+USA/data=!4m2!3m1!1s0x80859a6d00690021:0x4a501367f076adff?sa=X&ved=2ahUKEwjEiLP3-8frAhVTfX0KHZPxC9sQ8gEwK3oECB0QBg")
+})
